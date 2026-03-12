126515483fe836b55f59014da7e9fa6d12cb7767
vishal modanwal <vk076394@gmail.com>
2026-03-12 01:14:38 -0400

    name , number and email validate

--- jsform.js
+++ jsform.js
@@ -1,22 +1,33 @@
 let form = document.querySelector("form");
+let name = document.querySelector("#name").value;
+let email = document.querySelector("#email").value;
+let password =document.querySelector("#password").value;
+let confirmPassword = document.querySelector("#confirmPassword").value;
+let phone = document.querySelector("#phone").value;
+let error = document.querySelector("#error");
 
-let name = document.querySelector("#name");
-let email = document.querySelector("#email");
-let password =document.querySelector("#password");
-let confirmPassword = document.querySelector("#confirmPassword");
-let phone = document.querySelector("#phone");
+const Emails = new Set();
 
+
+//prevent once , validate everything
 form.addEventListener("submit" , function(e){
     e.preventDefault();
 
-    console.log(name.value );
+    console.log(name);
 
 //    if(name.value.length <=2){
 //     alert( "name is too short please entre valid name with more than 2 character ")
 //    }     
-nameChecker(name);
-validateEmail(email);
+   const validName = nameChecker(name);
+   const validEmail = validateEmail(email);
+
+  let valid = validName && validEmail ;
     
+    
+
+if(valid){
+    form.submit();
+}
     
 
 
@@ -24,14 +35,43 @@ validateEmail(email);
 })
 
 function nameChecker(name){
-   if(name.value.length <=2){
-    alert( "name is too short please entre valid name with more than 2 character ")
+   if(name.trim() === ""){
+    errorHappen("name is empty please fill it");
+    return false;
    }
+   if(name.length <=2){
+    errorHappen("name is too short please entre valid name with more than 2 character");
+    return false;  
+   }
+
+   return true;
 }
 
 function validateEmail(email){
-    const regex = ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$;
-    if(!regex.test(email)){
-        alert("please entre a valid email");
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    if(!emailRegex.test(email)){
+        errorHappen("please entre a valid email");
+        return false;
     }
+    if(Emails.has(email)){
+        errorHappen("email was already present please add unique mail");
+        return false;
+    }
+    Emails.add(email);
+    return true;
+}
+
+
+function numberValidate(phone){
+    const phoneRegex = /^[0-9]{10}$/;
+    if(!phoneRegex.test(phone)){
+      errorHappen("Phone must be 10 digits")
+       return false;
+    }
+    return true;
+  }
+
+
+function errorHappen(err){
+    error.textContent = err;
 }
